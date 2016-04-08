0cc8ccf707a605886446970e5fa8c6c6e7a6bd39
Boyko Andonov <Andonov>
2016-04-08 11:55:57 +0300

    Start Part 4

---
+++ Part.04/Part.04.entry_check.05.summation/stdafx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp : source file that includes just the standard includes
+// Part.04.entry_check.05.summation.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+
